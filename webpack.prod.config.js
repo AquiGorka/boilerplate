bf684d89f0cb38b249ff408f9826aad9fce3aaf7
Gorka <gorka@aquigorka.com>
2016-04-08 12:37:25 -0300

    Updates production build script

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -17,10 +17,11 @@ module.exports = {
       include: path.join(__dirname, 'src')
     }, {
       test: /\.styl$/,
-      loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!autoprefixer-loader!stylus-loader')
+      loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader!stylus-loader')
     }]
   },
   plugins: [
+    new webpack.DefinePlugin({ 'process.env': { 'NODE_ENV': JSON.stringify('production') } }),
     new ExtractTextPlugin( 'css/styles.css', {allChunks: false }),
     new webpack.optimize.UglifyJsPlugin({minimize: true}),
     new HtmlWebpackPlugin({ template: 'src/template/index.html', inject: false })
